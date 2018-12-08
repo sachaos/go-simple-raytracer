16f2db49a901456646ae0692368fe78a57ba00dc
Takumasa Sakao <takumasa.sakao@aktsk.jp>
2018-12-08 20:44:42 +0900

    Implement Metal

--- main.go
+++ main.go
@@ -1,22 +1,28 @@
 package main
 
 func main() {
-	height := 400
-	width := 400
+	height := 750
+	width := 1000
 
 	lookFrom := NewVector(-2, 0, 0.3)
 	lookAt := NewVector(2, 0, 0)
 	vup := NewVector(0, 0, 1)
-	c := NewCamera(lookFrom, lookAt, vup, 0.5, 1.0)
+	c := NewCamera(lookFrom, lookAt, vup, 0.5, 1.5)
 
-	sphere := &Sphere{NewVector(0, 0, 0.2), 0.2, &Lambertian{
+	sphere := &Sphere{NewVector(0, -0.25, 0.3), 0.3, &Metal{
+		albedo: &Vector{0.95, 0.95, 0.95},
+	}}
+	sphere2 := &Sphere{NewVector(0, 0.25, 0.2), 0.2, &Lambertian{
 		albedo: &Vector{0.5, 0.5, 0.9},
 	}}
+	sphere3 := &Sphere{NewVector(-0.5, 0, 0.15), 0.15, &Lambertian{
+		albedo: &Vector{0.5, 0.9, 0.5},
+	}}
 	ground := &Sphere{NewVector(0, 0, -100), 100, &Lambertian{
-		albedo: &Vector{0.9, 0.5, 0.5},
+		albedo: &Vector{0.5, 0.5, 0.5},
 	}}
 
-	objectList := &ObjectList{spheres: []*Sphere{sphere, ground}}
+	objectList := &ObjectList{spheres: []*Sphere{sphere, sphere2, sphere3, ground}}
 	scene := &Scene{sampleCount: 100, height: height, width: width, camera: c, objectList: objectList}
 
 	scene.Render()
--- material.go
+++ material.go
@@ -12,3 +12,12 @@ func (l *Lambertian) Scatter(ray *Ray, h *Hit) (*Ray, *Vector) {
 	target := h.P.Add(h.N).Add(NewRandomVectorInUnitSphere())
 	return &Ray{Origin: h.P, Dir: target.Sub(h.P)}, l.albedo
 }
+
+type Metal struct {
+	albedo *Vector
+}
+
+func (m *Metal) Scatter(ray *Ray, h *Hit) (*Ray, *Vector) {
+	dir := ray.Dir.Sub(h.N.Multi(ray.Dir.InnerProduct(h.N)).Multi(2))
+	return &Ray{Origin: h.P, Dir: Normalize(dir)}, m.albedo
+}
