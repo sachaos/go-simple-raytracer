aa93a3bde00efde81f3a2e8deda21378c24cdf2d
Takumasa Sakao <takumasa.sakao@aktsk.jp>
2018-12-18 14:46:13 +0900

    go fmt

--- main.go
+++ main.go
@@ -11,10 +11,10 @@ var sizeMin = 0.05
 var sizeMax = 0.3
 
 func genSphere(currentSpheres []*Sphere) *Sphere {
-	x := rand.Float64() * genRange * 2 - genRange
-	y := rand.Float64() * genRange * 2 - genRange
+	x := rand.Float64()*genRange*2 - genRange
+	y := rand.Float64()*genRange*2 - genRange
 
-	size := (sizeMax - sizeMin) * rand.Float64() + sizeMin
+	size := (sizeMax-sizeMin)*rand.Float64() + sizeMin
 	c := &Vector{rand.Float64(), rand.Float64(), rand.Float64()}
 	materialType := rand.Intn(2)
 	var material Material
@@ -44,7 +44,7 @@ func main() {
 	lookFrom := NewVector(-4.5, 0, 0.8)
 	lookAt := NewVector(0, 0, 0.25)
 	vup := NewVector(0, 0, 1)
-	c := NewCamera(lookFrom, lookAt, vup, 0.5, float64(width) / float64(height))
+	c := NewCamera(lookFrom, lookAt, vup, 0.5, float64(width)/float64(height))
 
 	var spheres []*Sphere
 	for i := 0; i < 300; i++ {
@@ -60,7 +60,7 @@ func main() {
 	spheres = append(spheres, ground)
 
 	objectList := &ObjectList{spheres: spheres}
-	scene := &Scene{sampleCount: 100, height: height, width: width, camera: c, objectList: objectList}
+	scene := &Scene{sampleCount: 5, height: height, width: width, camera: c, objectList: objectList}
 
 	scene.Render()
 }
